60094ebf642dc3c006965864602634218d5a373f
kazukiGames2112 <kubota1403@icloud.com>
2025-12-26 02:46:45 +0900

    ウィンドウ作成まで完了。ウィンドウが表示されたことを確認

--- Project1/Application.cpp
+++ Project1/Application.cpp
@@ -4,3 +4,24 @@ void Application::Initialize(HINSTANCE hInst)
 {
 	window_.Initialize(hInst);
 }
+
+void Application::Loop()
+{
+	while (true) {
+		bool result = window_.MessageLoop();
+		if (result == false)break;
+
+		result = gameloop();
+		if (result == false)break;
+	}
+}
+
+void Application::Terminate()
+{
+	window_.Terminate();
+}
+
+bool Application::gameloop()
+{
+	return true;
+}
